3e6632142adffb828680882b6746a9b41d001d3e
AsadMuxtorov <asadbekmuxtorov0@gmail.com>
2026-02-16 19:01:15 +0500

    Start full stack app and Navbar fixed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import logo from '../constants/assets/Zara_logo_1980.png'
+const Navbar = () => {
+	return (
+		<div class='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3'>
+			<Link to={'/'}>
+				<img src={logo} alt='Logo' width={200} />
+			</Link>
+			<nav class='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
+				<Link
+					className='me-3 py-2 link-body-emphasis text-decoration-none'
+					to={'/login'}
+				>
+					Login
+				</Link>
+				<Link
+					className='me-3 py-2 link-body-emphasis text-decoration-none'
+					to={'/register'}
+				>
+					Register
+				</Link>
+			</nav>
+		</div>
+	)
+}
+
+export default Navbar
