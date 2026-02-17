5ff576c31c26ebf6a9d2ba8321d0305e1c7dda07
AsadMuxtorov <asadbekmuxtorov0@gmail.com>
2026-02-17 18:31:44 +0500

    Redux & ReduxToolkit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import registerLogo from '../constants/assets/i (1).webp'
+import { loginUserStart } from '../slice/auth'
 import Input from '../ui/Input'
 const Login = () => {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
+	const dispatch = useDispatch()
+	const { isLoading } = useSelector(state => state.auth)
+
+	const loginHandler = e => {
+		e.preventDefault()
+		dispatch(loginUserStart())
+	}
 	return (
 		<div className='text-center mt-5'>
 			<main className='form-signin w-25 m-auto'>
@@ -19,8 +28,13 @@ const Login = () => {
 					<Input label={'Username'} state={name} setState={setName} />
 					<Input label={'Email address'} state={email} setState={setEmail} />
 
-					<button className='w-100 btn btn-lg btn-primary mt-2' type='submit'>
-						Log in
+					<button
+						className='w-100 btn btn-lg btn-primary mt-2'
+						disabled={isLoading}
+						type='submit'
+						onClick={loginHandler}
+					>
+						{isLoading ? 'loading...' : 'Login'}
 					</button>
 				</form>
 			</main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 import logo from '../constants/assets/Zara_logo_1980.png'
 const Navbar = () => {
 	return (
-		<div class='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3'>
+		<div className='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3'>
 			<Link to={'/'}>
 				<img src={logo} alt='Logo' width={200} />
 			</Link>
-			<nav class='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
+			<nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
 				<Link
 					className='me-3 py-2 link-body-emphasis text-decoration-none'
 					to={'/login'}
