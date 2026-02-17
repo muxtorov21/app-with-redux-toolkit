aaef6599df64434b034f7837aa74258119b92a21
AsadMuxtorov <asadbekmuxtorov0@gmail.com>
2026-02-17 16:55:08 +0500

    Login & register page & ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react'
+import registerLogo from '../constants/assets/i (1).webp'
+import Input from '../ui/Input'
 const Login = () => {
-	return <div>Login</div>
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	return (
+		<div className='text-center mt-5'>
+			<main className='form-signin w-25 m-auto'>
+				<form>
+					<img
+						src={registerLogo}
+						alt='rgister logo'
+						className='mb-2'
+						width={80}
+						height={80}
+					/>
+					<h1 className='h3 mb-3 fw-normal'>Please login</h1>
+					<Input label={'Username'} state={name} setState={setName} />
+					<Input label={'Email address'} state={email} setState={setEmail} />
+
+					<button className='w-100 btn btn-lg btn-primary mt-2' type='submit'>
+						Log in
+					</button>
+				</form>
+			</main>
+		</div>
+	)
 }
 
 export default Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react'
+import registerLogo from '../constants/assets/i (1).webp'
+import Input from '../ui/Input'
+
 const Register = () => {
-	return <div>Register</div>
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	return (
+		<div className='text-center mt-5'>
+			<main className='form-signin w-25 m-auto'>
+				<form>
+					<img
+						src={registerLogo}
+						alt='rgister logo'
+						className='mb-2'
+						width={80}
+						height={80}
+					/>
+					<h1 className='h3 mb-3 fw-normal'>Please register</h1>
+					<Input label={'Username'} state={name} setState={setName} />
+					<Input label={'Email address'} state={email} setState={setEmail} />
+					<Input
+						label={'Password'}
+						type='password'
+						state={password}
+						setState={setPassword}
+					/>
+					<button className='w-100 btn btn-lg btn-primary mt-2' type='submit'>
+						Register
+					</button>
+				</form>
+			</main>
+		</div>
+	)
 }
 
 export default Register
